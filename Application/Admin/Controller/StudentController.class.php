17b2af3edc94b153920322d80e87849967c40f95
wxy5123744 <wxy5123744@qq.com>
2014-12-06 12:39:56 +0800

    wxy
    Signed-off-by: wxy5123744 <wxy5123744@qq.com>

--- Application/Admin/Controller/StudentController.class.php
+++ Application/Admin/Controller/StudentController.class.php
@@ -2,10 +2,63 @@
 namespace Admin\Controller;
 use Think\Controller;
 class StudentController extends Controller {
+	/*
+		就业信息页面
+	*/
     public function stu_info(){
+		$data['action']='stu_insert';
+		$data['tag']='添加';
+		$this->assign($data);
 		$this->display();
     }
+	/*
+		就业信息列表页面
+	*/
 	 public function stu_list(){
 		 $this->display();
     }
+	/*
+		添加就业信息
+	*/
+	public function stu_insert(){
+		$upload = new \Think\Upload();// 实例化上传类
+		$upload->rootPath='Public/student/';
+		$upload->autoSub=false;
+		$upload->saveName=md5($_POST['name']);
+		$info=$upload->upload();    
+		if(!$info){
+			echo $upload->getError();    
+		}else{   
+			foreach($info as $file){        
+				$img='student/'.$file['savename'];
+				$User = M("student");
+				if($User->add(array(
+						'student_name'=>$_POST['name'],
+						'student_school'=>$_POST['school'],
+						'student_img'=>$img,
+						'job_time'=>$_POST['time'],
+						'job_company'=>$_POST['company'],
+						'job_money'=>$_POST['money']
+					))){
+					$this->info('成功','stu_info','继续添加','stu_list','查看列表');
+				}else{
+					$this->info('失败','','','stu_list','查看列表');
+				}
+			}
+		}
+    }
+	/*
+		操作信息页面
+	*/
+	 public function info($info,$url1,$href1,$url2,$href2){
+		 $data['info']=$info;
+		 $data['url1']=$url1;
+		 $data['href1']=$href1;
+		 $data['url2']=$url2;
+		 $data['href2']=$href2;
+		 $this->assign($data);
+		 $this->display('info');
+    }
+
+
 }
--- Application/Runtime/Cache/Admin/4b5dea502445fa17c7a3a75e0ebd27ae.php
+++ Application/Runtime/Cache/Admin/4b5dea502445fa17c7a3a75e0ebd27ae.php
@@ -1,16 +1,16 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<?php if (!defined('THINK_PATH')) exit();?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
 <head>
 	<meta http-equiv="Content-Type" content="text/html;charset=UTF-8" />
-		<link rel="stylesheet" href="__PUBLIC__/css/public.css" />
+		<link rel="stylesheet" href="/Public/css/public.css" />
 	<title>就业信息</title>
 </head>
 <body>
 
-	<form action="__URL__/{$action}" method="post" enctype="multipart/form-data">
+	<form action="/index.php/Admin/Student/<?php echo ($action); ?>" method="post" enctype="multipart/form-data">
 		<table class="table">
 			<tr>
-				<td class="th" colspan="2">{$tag}就业信息</td>
+				<td class="th" colspan="2"><?php echo ($tag); ?>就业信息</td>
 			</tr>
 			<tr>
 				<td width=5%>学员姓名</td>
