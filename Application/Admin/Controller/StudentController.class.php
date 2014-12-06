f841c721e16e6ae5411feb5534093175c7a8d387
wxy5123744 <wxy5123744@qq.com>
2014-12-06 09:59:23 +0800

    wxy
    Signed-off-by: wxy5123744 <wxy5123744@qq.com>

---
+++ Application/Admin/Controller/StudentController.class.php
@@ -0,0 +1,11 @@
+<?php
+namespace Admin\Controller;
+use Think\Controller;
+class StudentController extends Controller {
+    public function stu_add(){
+		echo "stu_add";
+    }
+	 public function stu_list(){
+		 echo "stu_list";
+    }
+}
