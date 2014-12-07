0a0f7a3db5fcc0c2dcfd0889b0814192f81c5106
hertar <809298222@qq.com>
2014-12-07 21:32:45 +0800

    shs Signed-off-by: hertar <809298222@qq.com>

--- Application/Runtime/Cache/Admin/4d8c21ded7bf044edc89b6dcb465f7f4.php
+++ Application/Runtime/Cache/Admin/4d8c21ded7bf044edc89b6dcb465f7f4.php
@@ -1,34 +1,36 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<?php if (!defined('THINK_PATH')) exit();?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
 <head>
         <meta http-equiv="Content-Type" content="text/html;charset=UTF-8" />
-                <link rel="stylesheet" href="__PUBLIC__/css/public.css" />
-                <script type="text/javascript" src="__PUBLIC__/admin/js/jq.js"></script>
+                <link rel="stylesheet" href="/Public/css/public.css" />
+                <script type="text/javascript" src="/Public/admin/js/jq.js"></script>
 
 </head>
 <body>
-    <form action="add_url_pro" method="post" onsubmit="return check()">
+    <form action="update_nav_pro" method="post" onsubmit="return check()">
                 <table class="table">
                         <tr >
-                                <td class="th" colspan="2">添加导航</td>
+                                <td class="th" colspan="2">修改导航</td>
                         </tr>
                         <tr>
                                 <td>导航名称</td>
                                 <td>
-                                    <input type="text" name="nav_name" onblur="checkname()" id="nav_name"/>
+                                    <input type="text" name="nav_name" onblur="checkname()" id="nav_name" value='<?php echo ($data["nav_name"]); ?>'/>
                                     <font color="red"><span id="spname"></span></font>
                                 </td>
                         </tr>
                           <tr>
                                 <td>导航url</td>
                                 <td>
-                                    <input type="text" name="nav_url" onblur="checkurl()" id="nav_url"/>
+                                   <input type='hidden' name='h_id' value='<?php echo ($data["nav_id"]); ?>'>
+                                    <input type="text" name="nav_url" onblur="checkurl()" id="nav_url" value="<?php echo ($data["nav_url"]); ?>"/>
                                     <font color="red"><span id="spurl"></span></font>
                                 </td>
                         </tr>
                         <tr>
                                 <td>开启状态</td>
                                 <td>
+                                    
                                         <input type="radio" name="is_show" value="1" checked="checked"/>开启
                                         <input type="radio" name="is_show" value="0" />关闭
                                 </td>
@@ -37,7 +39,7 @@
                         <tr>
                                 <td>排序</td>
                                 <td>
-                                <input type="text" name="sort" value="1"/>
+                                <input type="text" name="sort" value="<?php echo ($data["sort"]); ?>"/>
 
                                 </td>
 
@@ -46,7 +48,7 @@
 
                         <tr>
                                 <td colspan="2">
-                                        <input type="submit" value="添加" class="input_button"/>
+                                        <input type="submit" value="修改" class="input_button"/>
                                         <input type="reset" class="input_button"/>
                                 </td>
                         </tr>
--- Application/Runtime/Cache/Admin/81709e91097dee2ef14f7689a5b02ab0.php
+++ Application/Runtime/Cache/Admin/81709e91097dee2ef14f7689a5b02ab0.php
@@ -1,44 +1,65 @@
 <?php if (!defined('THINK_PATH')) exit();?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
 <head>
-	<meta http-equiv="Content-Type" content="text/html;charset=UTF-8" />
-	<title></title>
-	<link rel="stylesheet" href="/Public/css/public.css" />
-	<script type="text/javascript" src="/Public/js/jquery-1.7.2.min.js"></script>
-	<script type="text/javascript" src="/Public/js/public.js"></script>	
+        <meta http-equiv="Content-Type" content="text/html;charset=UTF-8" />
+        <title></title>
+        <link rel="stylesheet" href="/Public/css/public.css" />
+        <script type="text/javascript" src="/Public/js/jquery-1.7.2.min.js"></script>
+        <script type="text/javascript" src="/Public/js/public.js"></script>	
+         <script type="text/javascript" src="/Public/admin/js/jq.js"></script>
 </head>
 <body>
-	<table class="table">
-		<tr>
-			<td class="th" colspan="10">导航列表</td>
-		</tr>
-		<tr>
-			
-			<th>导航名称</th>
-			<th>URL</th>
-                                                          <th>提交</th>
-			<th>操作</th>
-		</tr>
-		<?php if(is_array($data)): $i = 0; $__LIST__ = $data;if( count($__LIST__)==0 ) : echo "" ;else: foreach($__LIST__ as $key=>$list): $mod = ($i % 2 );++$i;?><tr align='center'>
-			
-			
-			
-                                                               <td><?php echo ($list["nav_name"]); ?></td> 
+        <table class="table">
+                <tr>
+                        <td class="th" colspan="10">导航列表</td>
+                </tr>
+                <tr>
+
+                        <th>导航名称</th>
+                        <th>URL</th>
+                                                          <th>状态</th>
+                        <th>操作</th>
+                </tr>
+                <?php if(is_array($data)): $i = 0; $__LIST__ = $data;if( count($__LIST__)==0 ) : echo "" ;else: foreach($__LIST__ as $key=>$list): $mod = ($i % 2 );++$i;?><tr align='center'>
+                        <td><?php echo ($list["nav_name"]); ?></td> 
                                                                <td><?php echo ($list["nav_url"]); ?></td>
                                                                <?php if($list["is_show"] == 1): ?><td>开启</td>
                                                                <?php else: ?>
                                                                <td>关闭</td><?php endif; ?>
-			<td>
-                                                                <?php if($list["is_show"] == 1): ?><a href="">[关闭]</a>
+                        <td>
+                                                                <?php if($list["is_show"] == 1): ?><a href="/index.php/Admin/Url/show?id=<?php echo ($list["nav_id"]); ?>&is_show=<?php echo ($list["is_show"]); ?>" >[关闭]</a>
                                                                 <?php else: ?>
-                                                                              <a href="">[开启]</a><?php endif; ?>
-				<a href="">[编辑]</a>
-				<a href="" class="del">[删除]</a>
-			</td>
-		</tr><?php endforeach; endif; else: echo "" ;endif; ?>
-	</table>
-		<div class="page">
-			<a href="">1</a>
-		</div>
+                                                                     <a href="/index.php/Admin/Url/show?id=<?php echo ($list["nav_id"]); ?>&is_show=<?php echo ($list["is_show"]); ?>" >[开启]</a><?php endif; ?>
+                                <a href="/index.php/Admin/Url/update_nav?id=<?php echo ($list["nav_id"]); ?>">[编辑]</a>
+                                <a href="/index.php/Admin/Url/del?id=<?php echo ($list["nav_id"]); ?>" class="del">[删除]</a>
+                        </td>
+                </tr><?php endforeach; endif; else: echo "" ;endif; ?>
+        </table>
+                <div class="page">
+                        <?php echo ($page); ?>
+                </div>
 </body>
 </html>
+<!--
+<script>
+
+  function is_show(is_show,id){
+      //  alert("/index.php/Admin/Url/is_show")
+        $.ajax({
+                url:'/index.php/Admin/Url/show',
+                data:{'is_show':is_show,'id':id},
+                type:'post',
+                success:function(e){
+                        alert(e)
+                        if (e==1){
+                                  $("#img"+n_id).attr("src","<?php echo $public?>/images/error.gif");
+
+                                }else{
+                                        $("#img"+n_id).attr("src","<?php echo $public?>/images/succ.png");
+
+                        }
+                }
+        })
+    }
+</script>
+-->
