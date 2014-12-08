02eea875a9d34801ca63477c905e7a90d6c75a36
hertar <809298222@qq.com>
2014-12-08 19:23:55 +0800

    史宏爽
    Signed-off-by: hertar <809298222@qq.com>

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -6,13 +6,23 @@ class IndexController extends Controller {
         $model=M('nav');
         $data=$model->where('is_show=1')->order('sort')->select();
         $data1=$this->kaibanxinxi();
+        $data2=$this->jiuyexinxi();
         $this->assign('data',$data);
         $this->assign('data1',$data1);
+        $this->assign('data2',$data2);
         $this->display();
     }
-    
+    //开班信息
     public function kaibanxinxi(){
           $model=M('class');
+          $data=$model->select();
+          return $data;
+    }
+    
+    //就业信息
+    
+    public function jiuyexinxi(){
+          $model=M('student');
           $data=$model->select();
           return $data;
     }
