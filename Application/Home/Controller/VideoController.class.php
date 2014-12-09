5c3d5c23b3a36ee79ffbd1d056d24bec8b31901e
lvchen <1003639438@qq.com>
2014-12-09 08:21:07 +0800

    前台视频播放添加分页

--- Application/Home/Controller/VideoController.class.php
+++ Application/Home/Controller/VideoController.class.php
@@ -1,16 +1,21 @@
 <?php
 namespace Home\Controller;
 use Think\Controller;
+
 class VideoController extends Controller {
     //php视频列表
 	public function index(){
-		$video=M('video');
-		$data=$video->select();
-		$this->assign('arr',$data);
-		$models=M('nav');
-		$data=$models->where('is_show=1')->order('sort')->select();
-		$this->assign('data',$data);
-		$this->display();	
+            $video=M('video');
+            $count= $video->count();
+            $Page = new \Think\Page($count,10);
+            $show= $Page->show();
+            $list = $video->limit($Page->firstRow.','.$Page->listRows)->select();
+            $this->assign('arr',$list);
+            $this->assign('page',$show);
+            $models=M('nav');
+            $data=$models->where('is_show=1')->order('sort')->select();
+            $this->assign('data',$data);
+            $this->display();	
     }
 	//播放视频
 	public function play(){
