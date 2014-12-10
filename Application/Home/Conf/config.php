84ba0e0153ec245e010adf33efc79eada6cd5474
lvchen <1003639438@qq.com>
2014-12-10 18:31:59 +0800

    静态缓存

--- Application/Home/Conf/config.php
+++ Application/Home/Conf/config.php
@@ -2,5 +2,12 @@
 return array(
 	//'配置项'=>'配置值'
     'LAYOUT_ON'=>true,
-    'LAYOUT_NAME'=>'public/layout',
+    'LAYOUT_NAME'=>'public/layout', 
+    'HTML_CACHE_ON'     =>    true, // 开启静态缓存
+    'HTML_CACHE_TIME'   =>    60,   // 全局静态缓存有效期（秒）
+    'HTML_FILE_SUFFIX'  =>    '.html',//设置静态缓存文件后缀
+    'HTML_CACHE_RULES'  =>     array(  // 定义静态缓存规则  
+       // 定义格式1 数组方式   
+     'Index:'=>array('Index/{:action}_{id}','60'),  
+      )
 );
